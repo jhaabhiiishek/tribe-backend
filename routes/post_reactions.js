1d40639b3f37d35a1d5deef96cec0763d22f5b40
Abhishek Jha <abhi10092003@gmail.com>
2024-01-20 18:02:47 +0530

    solved onliked error

--- routes/post_reactions.js
+++ routes/post_reactions.js
@@ -104,7 +104,7 @@ app.post('/upvote',authenticate, async function(req, res, next) {
 				user_id : posted_by,
 				type:'liked',
 				action_performed_by:user_id,
-				action_on_User_post_id : user_post_id
+				action_on_post_id : user_post_id
 			})
 			return res.status(201).json({
 				success:1,
